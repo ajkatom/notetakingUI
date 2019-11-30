c40489a82cea55e1822aee14898c3feca00876df
ajkatom <alonadelson@alons-MacBook-Pro.local>
2019-11-30 18:09:47 -0500

    GQL Error handling

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloLink, ApolloClient } from 'apollo-boost';
-//import {  } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import { onError } from 'apollo-link-error';
+import Notification, { notify } from 'react-notify-toast';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ massage }) => {
+      let msg = massage;
+      return notify.show(msg, 'error');
+    });
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4300/graphql',
@@ -19,12 +29,14 @@ const httpLink = createHttpLink({
 });
 const link = ApolloLink.from([httpLink]);
 const client = new ApolloClient({
+  errorLink,
   link,
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <Notification />
     <App />
   </ApolloProvider>,
   document.getElementById('root')
